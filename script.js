518a8f9ba850e7533459f1944d6c29b85b7f3bd8
adagustafik <ada.gustafik@gmail.com>
2021-10-26 09:32:09 +0200

    Fixed position of last paragraph and title - similar to other paragraphs

--- script.js
+++ script.js
@@ -8,5 +8,5 @@ let additionalBlock = {
     title: "Added with javascript",
     text: "This block was added using JavaScript's jQuery library. How awesome!"
 };
-$('ul').last().append('<h3>'+ additionalBlock.title + '</h3>');
-$('ul').last().append('<p>'+ additionalBlock.text + '</p>');
+$('body').append('<h2>'+ additionalBlock.title + '</h2>');
+$('body').append('<p>'+ additionalBlock.text + '</p>');
